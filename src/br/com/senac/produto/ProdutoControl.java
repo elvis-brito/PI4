e01f375fc495a7866a243cbb5b17c1f31c3802fb
elvis.bsantos <elvis.bsantos@CAS554519W10G10.senacsp.edu.br>
2018-05-08 20:26:28 -0300

    correção

--- src/br/com/senac/produto/ProdutoControl.java
+++ src/br/com/senac/produto/ProdutoControl.java
@@ -14,7 +14,7 @@ public class ProdutoControl {
     ResultSet rs;
     
     String produtoInsert = "INSERT INTO produtos (nome, genero, descricao, estoque, data_produto, valor) VALUES (?,?,?,?,?)";
-    String produtoSelect = "SELECT * FROM produto WHERE nome LIKE ?";
+    String produtoSelect = "SELECT * FROM produtos WHERE nome LIKE ?";
     
     public void CadastrarProduto(Produto produto){
         try{
@@ -33,21 +33,21 @@ public class ProdutoControl {
             ex.printStackTrace();
         }
     }
-    public List<ProdutoBean> listaProduto(String NOME_PROD){
-        List<ProdutoBean> produto = new ArrayList();
+    public List<Produto> listaProduto(String NOME_PROD){
+        List<Produto> produto = new ArrayList();
         try{
-            AcessoMySql mysql = new AcessoMySql();
+            Produto mysql = new Produto();
             pstm = mysql.conectar().prepareStatement(produtoSelect);
             pstm.setString(1, NOME_PROD);
             rs = pstm.executeQuery();
-            ProdutoBean prod;
+            Produto prod;
             while(rs.next()){
-                prod = new ProdutoBean();
-                prod.setCOD_PROD(rs.getInt("COD_PROD"));
-                prod.setNOME_PROD(rs.getString("NOME_PROD"));
-                prod.setTIPO_PROD(rs.getString("TIPO_PROD"));
-                prod.setVL_PROD(rs.getDouble("VL_PROD"));
-                prod.setQTD_PROD(rs.getInt("QTD_PROD"));
+                prod = new Produto();
+                prod.setCodigo(rs.getInt("COD_PROD"));
+                prod.setNome(rs.getString("NOME_PROD"));
+                prod.setDescricao(rs.getString("TIPO_PROD"));
+                prod.setValor(rs.getDouble("VL_PROD"));
+                prod.setQuantidade(rs.getInt("QTD_PROD"));
                 produto.add(prod);
             }
         }catch(Exception ex){
@@ -56,46 +56,21 @@ public class ProdutoControl {
         return produto;
         }
     String produtoUpdate = "UPDATE produto SET NOME_PROD = ?, TIPO_PROD = ?, VL_PROD = ?, QTD_PROD = ? WHERE COD_PROD = ?";
-    public void alterarProduto(ProdutoBean produto){
+    public void alterarProduto(Produto produto){
         try{
-            AcessoMySql mysql = new AcessoMySql();
+            Produto mysql = new Produto();
             pstm = mysql.conectar().prepareStatement(produtoUpdate);
-            pstm.setString(1, produto.getNOME_PROD());
-            pstm.setString(2, produto.getTIPO_PROD());
-            pstm.setDouble(3, produto.getVL_PROD());
-            pstm.setInt(4, produto.getQTD_PROD());
-            pstm.setInt(5, produto.getCOD_PROD());
+            pstm.setString(1, produto.getNome());
+            pstm.setString(2, produto.getDescricao());
+            pstm.setDouble(3, produto.getValor());
+            pstm.setInt(4, produto.getQuantidade());
+            pstm.setInt(5, produto.getCodigo());
             pstm.executeUpdate();
         }catch(SQLException ex){
             ex.printStackTrace();
         }
     }
-    
-    
-    
-    
-    //public static void inserir(Produto produto) throws Exception {
-        Connection con = null;
-        try {
-            con = ConnectionUtils.getConnection();
-
-            String sql = "";
-            PreparedStatement ps = con.prepareStatement(sql);
-
-            ps.setString(1, produto.getNome());
-            ps.setString(2, produto.getGenero());
-            ps.setString(3, produto.getDescricao());
-            ps.setInt(4, produto.getQuantidade());
-            ps.setDate(5, new Date(produto.getData().getTime()));
-            ps.setDouble(6, produto.getValor());
-            ps.setBoolean(5, true);
-
-            ps.execute();
-        } finally {
-          if (con != null && !con.isClosed()){
-              con.close();
-          }
-        }
-
-    }
 }
+    
+    
+   
